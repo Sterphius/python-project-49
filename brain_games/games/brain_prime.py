d944ae17907f9c46ab60042786ca46743da470ba
Sterphius <malyshff@gmail.com>
2023-03-02 18:42:49 +0300

    formatting to str() for consistency

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -8,7 +8,7 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def generate_question_and_answer() -> Tuple[str, str]:
     rnd_num = randint(1, 100)
 
-    question = f'{rnd_num}'
+    question = str(rnd_num)
 
     answer = 'yes' if is_prime_number(rnd_num) is True else 'no'
 
